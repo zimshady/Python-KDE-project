2530035ebe9e94c30b21bce6e84ba20980e3d97c
Ton <zimshady6@gmail.com>
2016-11-06 18:06:07 +0000

    Added check of area under curves and tried to fit polynomial curve

--- KDE_subtraction_StatsModel.py
+++ KDE_subtraction_StatsModel.py
@@ -24,29 +24,35 @@ x_grid = np.linspace(0, 4000, 4000)
 
 #Import data from csv file
 def getdata ():
-    with open('BF11Ages1s.csv', 'rb') as csvfile:
+    with open('C:\GitHub Folders\Python-KDE-project\BF11Ages1s.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile)
         ages = [map(float, row) for row in reader]
 
         return ages
 
 def getmoredata ():
-    with open('BF12Ages1s.csv', 'rb') as csvfile:
+    with open('C:\GitHub Folders\Python-KDE-project\BF12Ages1s.csv', 'rb') as csvfile:
         reader = csv.reader(csvfile)
         ages = [map(float, row) for row in reader]
 
         return ages
 
 #acquire KDE and subtract
-pdf = kde_statsmodels_m(getdata(), x_grid, bandwidth=20)
-pdf2 = kde_statsmodels_m(getmoredata(), x_grid, bandwidth=20)
+pdf = kde_statsmodels_u(getdata(), x_grid, bandwidth=20)
+pdf2 = kde_statsmodels_u(getmoredata(), x_grid, bandwidth=20)
 difference = [a - b for a,b in zip(pdf,pdf2)]
+print np.trapz(pdf)
+print np.trapz(pdf2)
+best_fit = np.polyfit(x_grid,pdf,70)
 
+np.savetxt('C:\GitHub Folders\Python-KDE-project\pdf.txt',pdf)
 bf11, = plt.plot(x_grid, pdf, label="BF11",color='blue', alpha=0.5, lw=1)
 bf12, = plt.plot(x_grid, pdf2, label="BF12",color='red', alpha=0.5, lw=1)
 diff, = plt.plot(x_grid, difference, label="Difference", color='purple', alpha=0.5, lw=1)
+best, = plt.plot(x_grid, np.poly1d(best_fit)(x_grid), label="Best fit", color='black', alpha=0.5,lw=2)
 
 plt.legend()
 plt.title("BF11-BF12")
 plt.xlim(0, 4000)
-plt.savefig('univariate.pdf')
+plt.show()
+#plt.savefig('univariate.pdf')
